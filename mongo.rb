b918db49632c937fb73e1cb8cc444f67be6769d1
Tony Issakov <tissakov@thefrontiergroup.com.au>
2010-10-26 17:29:30 +0800

    trying to build up a mongo dep

---
+++ mongo.rb
@@ -0,0 +1,11 @@
+dep 'mongodb source' do
+  met? {  shell("cat /etc/apt/sources").grep(/downloads.mongodb.org/).empty? } 
+  meet {  
+    append_to_file "deb http://downloads.mongodb.org/distros/ubuntu 10.4 10gen", "/etc/apt/sources", :sudo=>true 
+  }
+end
+
+dep 'mongodb' do
+  requires 'mongodb source'
+  
+end
