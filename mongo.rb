25f58387b2c6fb712592276aef722f0b14163bf3
Tony Issakov <tissakov@thefrontiergroup.com.au>
2010-10-26 17:32:44 +0800

    grep fix

--- mongo.rb
+++ mongo.rb
@@ -1,5 +1,5 @@
 dep 'mongodb source' do
-  met? {  shell("cat /etc/apt/sources").grep(/downloads.mongodb.org/).empty? } 
+  met? {  shell("cat /etc/apt/sources").split("\n").grep(/downloads.mongodb.org/).empty? } 
   meet {  
     append_to_file "deb http://downloads.mongodb.org/distros/ubuntu 10.4 10gen", "/etc/apt/sources", :sudo=>true 
   }
