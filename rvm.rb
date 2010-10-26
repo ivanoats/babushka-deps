f0d1dae7a7e14bbbe68caebdcab83020ddebb68a
Tony Issakov <tissakov@thefrontiergroup.com.au>
2010-10-26 12:59:10 +0800

    add system wide install

--- rvm.rb
+++ rvm.rb
@@ -1,3 +1,9 @@
+# set up the system libraries
 dep 'rvm requirements' do 
   requires %w(build-essential.managed bison.managed openssl.managed libreadline5.managed libreadline_dev.managed zlib1g.managed libssl_dev.managed libsqlite3.managed libxml2-dev.managed)  
+end
+
+dep 'rvm system-wide' do
+  met? { File.exist?("/usr/local/bin/rvm") }
+  meet { sudo "bash < <( curl -L http://bit.ly/rvm-install-system-wide )" }
 end
