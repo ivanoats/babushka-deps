57c78d39bb781774769dd5389b1cabfcbfa4fc50
Tony Issakov <tissakov@thefrontiergroup.com.au>
2010-10-27 13:05:43 +0800

    attempt to add config to capify

--- capistrano.rb
+++ capistrano.rb
@@ -5,9 +5,7 @@ dep "capify" do
   }
   meet {
     in_dir(var(:rails_root)) { 
-      shell("capify .")
-      shell("mkdir config/deploy")
-      shell("touch config/deploy/staging.rb")
+      shell("capify .")      
     }
   }
 end
@@ -19,4 +17,18 @@ dep "capistrano bundler setup" do
   meet {
     append_to_file('gem "capistrano"\ngem "capistrano-ext"', "#{var(:rails_root)}/Gemfile")
   }  
+end
+
+def "capistrano config files" do
+  met? {
+    in_dir(var(:rails_root)) { File.exist?("config/deploy") && File.exist?("config/deploy/staging.rb")
+  }
+  meet {
+    in_dir(var(:rails_root)) { 
+      shell("mkdir -p config/deploy") 
+      shell("mv config/deploy.rb config/deploy.rb.#{Time.now}")
+    }
+    render_erb "capistrano/staging", :to=>"#{var(:rails_root)}/config/deploy/staging.rb"
+    render_erb "capistrano/main", :to=>"#{var(:rails_root)}/config/deploy.rb"
+  }
 end
