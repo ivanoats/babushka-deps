28feb6e6c45fb42a9bd391f0651a99a36cd82901
Tony Issakov <tissakov@thefrontiergroup.com.au>
2011-02-28 12:37:07 +0800

    working on rvm and servers

--- tfg.rb
+++ tfg.rb
@@ -29,7 +29,8 @@ dep 'tfg vhost bundle install' do
   met? { @actioned = false}
   meet { 
     Dir["/var/vhosts/*"].each do |dir|      
-      shell("cd #{dir}; bash .rvmrc; bundle install")
+      log_shell("Bundling #{dir}...","cd #{dir}; bash .rvmrc; bundle install")
     end
+    @actioned true
   }
 end
