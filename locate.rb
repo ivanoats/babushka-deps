b13a37998d8dc6ff361290e3827b79765ef28039
Ivan Storck <ivanoats@uw.edu>
2011-10-20 13:31:11 -0700

    locate only for osx

--- locate.rb
+++ locate.rb
@@ -1,5 +1,5 @@
 #os x only for now
-dep 'locate' do
+dep 'locate', :for => :osx do
   met? { shell "ls /var/db/locate.database" }
   meet :on => :osx do
     shell "sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist"
